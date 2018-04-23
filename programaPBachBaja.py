a89887d09d91f196efcf2b1346f02cebaf22f7ff
fcAlvarez10 <fc.alvarez10@uniandes.edu.co>
2018-04-22 23:00:37 -0500

    modificacion bridges

--- programaPBachBaja.py
+++ programaPBachBaja.py
@@ -2,6 +2,35 @@ import json
 import requests
 from kafka import KafkaConsumer
 
+# Configuration Values
+domain = "isis2503-jcgloria.auth0.com" # Your Auth0 Domain
+api_identifier = "uniandes.edu.co/thekeys" # API Identifier of your API
+client_id = "njxtvdPndBOMDB8eTBdWsi5YYnOtFuDm" # Client ID of your Non Interactive Client
+client_secret = "qNp8_GngN8ltWfsci_npGUGyEyrdr7cp6bA3Sj8384-4fzE4dxvlNvrEzPYqmaGT" # Client Secret of your Non Interactive Client
+grant_type = "http://auth0.com/oauth/grant-type/password-realm" # OAuth 2.0 flow to use
+scope = "openid"
+realm = "Username-Password-Authentication"
+username = "cerra1@yalelock.co"
+password ="Nolohagas1234"
+
+# Get an access token from Auth0
+base_url = "https://{domain}".format(domain=domain) + "/oauth/token"
+
+payload = {
+    'client_id': client_id,
+    'client_secret': client_secret,
+    'audience': api_identifier,
+    'grant_type': grant_type,
+    'realm': realm,
+    'username': username,
+    'password': password
+}
+response = requests.post(base_url, data=json.dumps(payload), headers={'Content-type': 'application/json'})
+
+auth0_response = response.json()
+print("Access Token: " + auth0_response.get('access_token'))
+print("Response Status Code: " + str(response.status_code))
+
 consumer = KafkaConsumer('alarma.conjunto1.baja.1-101.pre',
                          bootstrap_servers=['172.24.42.103:8090'])
 
@@ -12,5 +41,5 @@ for message in consumer:
     directorio = message.topic.split('.')
     ubicacion=directorio[3]
     url = "http://172.24.42.79:8080/inmuebles/"+ubicacion+"/alarmas"
-    response = requests.post(url,data_string,headers={'Content-type': 'application/json'})
+    response = requests.post(url,data_string,headers={'Content-type': 'application/json','Authorization':'Bearer '+auth0_response.get('access_token')})
     print("Response Status Code: " + str(response.status_code))
