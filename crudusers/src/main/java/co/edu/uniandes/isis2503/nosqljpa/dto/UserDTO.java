d8faae20c734887e7d062ba30dd8a8325cf0936c
JUAN CARLOS GLORIA CARBONELL <jc.gloria@sis.virtual.uniandes.edu.co>
2018-04-25 16:25:53 -0500

    post y delete de usuarios

--- crudusers/src/main/java/co/edu/uniandes/isis2503/nosqljpa/dto/UserDTO.java
+++ crudusers/src/main/java/co/edu/uniandes/isis2503/nosqljpa/dto/UserDTO.java
@@ -1,36 +1,10 @@
-/*
- * The MIT License
- *
- * Copyright 2018 Universidad De Los Andes - Departamento de Ingeniería de Sistemas.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package co.edu.uniandes.isis2503.nosqljpa.dto;
 
-/**
- *
- * @author mf.mena
- */
 public class UserDTO {
     
     private String correo;
     private String contraseña;
+    private String rol;
     
     public UserDTO(){
         
@@ -56,6 +30,12 @@ public class UserDTO {
     public void setContraseña(String contraseña) {
         this.contraseña = contraseña;
     }
-    
-    
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }   
 }
