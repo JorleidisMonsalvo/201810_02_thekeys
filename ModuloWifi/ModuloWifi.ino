68519bfed9a25cb9bad879e5d3ef344db915aa4d
rafafor24 <31598934+rafafor24@users.noreply.github.com>
2018-05-11 02:34:06 -0500

    Actualización Modulo

--- ModuloWifi/ModuloWifi.ino
+++ ModuloWifi/ModuloWifi.ino
@@ -5,10 +5,11 @@
 #define TOPIC_SUBSCRIBE        "claves.conjunto1.1-101"
 #define TOPIC_PUBLISH_1        "alarma.conjunto1.alta.1-101.pre"
 #define TOPIC_PUBLISH_2        "alarma.conjunto1.baja.1-101.pre"
+#define TOPIC_HEALTHCHECK      "healthcheck.conjunto1.1-101"
 #define SIZE_BUFFER_DATA       50
 
 //VARIABLES
-const char* idDevice = "ISIS2503";
+const char* idDevice = "ISIS2503ESXASDA";
 boolean     stringComplete = false;
 boolean     init_flag = false;
 String      inputString = "";
@@ -19,12 +20,12 @@ WiFiClient    clientWIFI;
 PubSubClient  clientMQTT(clientWIFI);
 
 // CONFIG WIFI
-const char* ssid = "isis2503";
-const char* password = "Yale2018.";
+const char* ssid = "FORERO";
+const char* password = "6764435r";
 
 // CONFIG MQTT
-IPAddress serverMQTT (172,24,42,103);
-const uint16_t portMQTT = 8083;
+IPAddress serverMQTT (192,168,0,7);
+const uint16_t portMQTT = 1883;
 // const char* usernameMQTT = "admin";
 // const char* passwordMQTT = "admin";
 
@@ -59,21 +60,25 @@ void reconnectWIFI() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
+  //Serial.println("Callback");
+  //Serial.println(length);
   Serial.write(payload, length);
   Serial.println();
 }
 
 void setup() {
   Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
   inputString.reserve(100);
-
+  connectWIFI();
   clientMQTT.setServer(serverMQTT, portMQTT);
   clientMQTT.setCallback(callback);
-  connectWIFI();
+  
   delay(1000);
 }
 
 void processData() {
+  
   if (WiFi.status() == WL_CONNECTED) {
     if(init_flag == false) {
       init_flag = true;
@@ -92,19 +97,30 @@ void processData() {
 
       if(conectMQTT) {
         if(clientMQTT.subscribe(TOPIC_SUBSCRIBE)) {
-          // Serial.println("Subscribe OK");
+           //Serial.println("Subscribe OK1");
+        }
+        if(clientMQTT.subscribe(TOPIC_HEALTHCHECK)) {
+           //Serial.println("Subscribe OK2");
         }
       }
     }
 
     if (stringComplete && clientMQTT.connected()) {
-      if(inputString == "1") {
+      if(inputString == "1\n") {
         if(clientMQTT.publish(TOPIC_PUBLISH_2, bufferData)) {
           inputString = "";
           stringComplete = false;
         }
       }
-      else {
+      else if(inputString== "0\n") 
+      {
+        if(clientMQTT.publish(TOPIC_HEALTHCHECK, bufferData)) {
+          inputString = "";
+          stringComplete = false;
+        }
+      }
+      else
+      {        
         if(clientMQTT.publish(TOPIC_PUBLISH_1, bufferData)) {
           inputString = "";
           stringComplete = false;
@@ -117,10 +133,14 @@ void processData() {
     connectWIFI();
     init_flag = false;
   }
+   if (!clientMQTT.connected()) {
+    reconnect();
+  }
   clientMQTT.loop();
 }
 
 void receiveData() {
+  
   while (Serial.available()) {
     // get the new byte:
     char inChar = (char)Serial.read();
@@ -132,9 +152,36 @@ void receiveData() {
     }
   }
 }
-
+void reconnect() {
+  // Loop until we're reconnected
+  while (!clientMQTT.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (clientMQTT.connect(idDevice)) {
+      if(clientMQTT.subscribe(TOPIC_SUBSCRIBE)) {
+           //Serial.println("Subscribe OK1");
+        }
+        if(clientMQTT.subscribe(TOPIC_HEALTHCHECK)) {
+           //Serial.println("Subscribe OK2");
+        }
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      //client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      //client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(clientMQTT.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 void loop() {
   receiveData();
   processData();
 }
-
