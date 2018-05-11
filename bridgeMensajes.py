93be601344e800bee74a206a0959965bbc7ea881
JUAN CAMILO SANGUINO PEREZ <jc.sanguino10@sis.virtual.uniandes.edu.co>
2018-05-10 23:00:41 -0500

    modificacion py de mensajeria (silenciar alarmas)

--- bridgeMensajes.py
+++ bridgeMensajes.py
@@ -6,14 +6,17 @@ consumer = KafkaConsumer('alarma.conjunto1.baja.1-101',
                          bootstrap_servers=['172.24.42.103:8090'])
 for message in consumer:
  json_data = json.loads(message.value.decode('utf-8'))
- correo= json_data.get('correo')
- asunto= json_data.get('asunto')
- cuerpo=json_data.get('cuerpo')
- url = 'http://172.24.42.45:8080/correo'
- payload = {
- 'correo': correo,
- 'asunto': asunto,
- 'cuerpo': cuerpo
- }
- response = requests.post(url, data=json.dumps(payload),headers={'Content-type': 'application/json'})
- print("Response Status Code: "+ str(response.status_code))
+ if json_data.get('seNotifica')==false:
+     pass
+ else:
+     correo= json_data.get('correo')
+     asunto= json_data.get('asunto')
+     cuerpo=json_data.get('cuerpo')
+     url = 'http://172.24.42.45:8080/correo'
+     payload = {
+     'correo': correo,
+     'asunto': asunto,
+     'cuerpo': cuerpo
+     }
+     response = requests.post(url, data=json.dumps(payload),headers={'Content-type': 'application/json'})
+     print("Response Status Code: "+ str(response.status_code))
